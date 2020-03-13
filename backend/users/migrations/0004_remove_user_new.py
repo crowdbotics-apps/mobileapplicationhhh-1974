02e698e31dccca77eaa6af84cdc7a2b073e064fe
Crowdbotics <team@crowdbotics.com>
2020-03-13 08:02:22 +0000

    Update models

--- backend/users/migrations/0004_remove_user_new.py
+++ backend/users/migrations/0004_remove_user_new.py
@@ -6,12 +6,9 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0003_user_new'),
+        ("users", "0003_user_new"),
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='user',
-            name='new',
-        ),
+        migrations.RemoveField(model_name="user", name="new",),
     ]
