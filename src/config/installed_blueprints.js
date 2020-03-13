86adf27874f11f22f92e3de4b10e226461970c7f
Crowdbotics <team@crowdbotics.com>
2020-03-13 08:22:07 +0000

    add package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'UserProfile549', human_name: 'User Profile', access_route: 'UserProfile549'},
 { name: 'UserProfile548', human_name: 'User Profile', access_route: 'UserProfile548'},
 { name: 'EmailAuth546', human_name: 'Email Auth', access_route: 'EmailAuth546', icon: 'envelope-o'},
 { name: 'EmailAuth544', human_name: 'Email Auth', access_route: 'EmailAuth544', icon: 'envelope-o'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import UserProfile549Navigator from '../features/UserProfile549/navigator';
 import UserProfile548Navigator from '../features/UserProfile548/navigator';
 import EmailAuth544Navigator from '../features/EmailAuth544/navigator';
 import EmailAuthNavigator from '../features/EmailAuth/navigator';
@@ -18,6 +19,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+UserProfile549: { screen: UserProfile549Navigator },
 UserProfile548: { screen: UserProfile548Navigator },
 EmailAuth544: { screen: EmailAuth544Navigator },
 EmailAuth: { screen: EmailAuthNavigator },
